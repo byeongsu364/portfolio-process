c1da976a995612603bf6b888a4dd97ee8b990ccc
byeongsu364 <kbsyjysbs@gmail.com>
2025-09-18 18:46:22 +0900

    ㄴㄴㄴ

--- frontend/src/components/home/Aboutme.jsx
+++ frontend/src/components/home/Aboutme.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./styles/Aboutme.scss"
 import aboutMe from '../../utils/aboutme'
+
 const Aboutme = () => {
   const profile = aboutMe.basic
   const skills = aboutMe.skills
   const exper = aboutMe.experiences
   const interests = aboutMe.interests
+
   return (
     <div className='inner aboutme-inner'>
       <h1 className="tit">
@@ -13,9 +15,7 @@ const Aboutme = () => {
         aboutMe
       </h1>
       <div className="t-wrap">
-        <h2 className="sub-tit">
-          profile
-        </h2>
+        <h2 className="sub-tit">profile</h2>
         <p className="txt">
           들어가는 최종 요소나 사용된다. <br />
           레이아웃 들어가는 로렘 부르며,
@@ -24,43 +24,38 @@ const Aboutme = () => {
       </div>
 
       <div className="about-container">
-        <div className="in-wrap profile-wrap">
-          <h4 className='list-lst'>profile</h4>
+        <div className="in-wrap">
+          <h4 className='list-lst'>list-tit</h4>
           <ul className="lst">
-            <li>
-              <strong>이름</strong>: {profile.name}
-            </li>
-            <li>
-              <strong>나이</strong>: {profile.age}
-            </li>
-            <li>
-              <strong>사는곳</strong>: {profile.location}
-            </li>
-            <li>
-              <strong>MBTI</strong>: {profile.mbti}
-            </li>
+            <li><strong>이름</strong> : {profile.name}</li>
+            <li><strong>나이</strong> : {profile.age}</li>
+            <li><strong>사는곳</strong> : {profile.location}</li>
+            <li><strong>MBTI</strong> : {profile.mbti}</li>
           </ul>
         </div>
+
         <div className="in-wrap skills-wrap">
           <h4 className='list-lst'>Skills</h4>
           <ul className="lst">
-            {skills.map((skill,i)=>(
+            {skills.map((skill, i) => (
               <li key={i}>{skill}</li>
             ))}
           </ul>
         </div>
+
         <div className="in-wrap exper-wrap">
-          <h4 className='list-lst'>      💼 Experiences</h4>
+          <h4 className='list-lst'>Experiences</h4>
           <ul className="lst">
-            {exper.map((exp,i)=>(
+            {exper.map((exp, i) => (
               <li key={i}>{exp}</li>
             ))}
           </ul>
         </div>
+
         <div className="in-wrap interests-wrap">
-          <h4 className='list-lst'>           🌱 Interests</h4>
+          <h4 className='list-lst'>Interests</h4>
           <ul className="lst">
-            {interests.map((int,i)=>(
+            {interests.map((int, i) => (
               <li key={i}>{int}</li>
             ))}
           </ul>
